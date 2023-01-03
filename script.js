c3af767bba7c28b86747ebb85b131763682e121a
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-03 18:36:36 +0000

    Make changes suggested by reviewer

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const hamburger = document.querySelector('.hamburger');
 const hamburgerX = document.querySelector('.hamburger-X');
 const  navUl = document.querySelector('.nav-ul');
 const navLinks = document.querySelectorAll('.nav-items-a');
+const projectBtn = document.querySelectorAll('.see-project-button');
+const portfolioName = document.querySelectorAll('#portfolio-name')[0];
+
+
+console.log(portfolioName)
 
 hamburger.addEventListener("click", () => {
     const menuItems = navUl.classList.toggle('active');
     const togglebtn = hamburger.classList.toggle('active');
     
     if (togglebtn && menuItems) {
+        portfolioName.style.display = 'none';
         hamburger.style.display ='none';
         hamburgerX.style.display = 'block';
         navUl.style.display = 'flex';
@@ -17,8 +23,10 @@ hamburger.addEventListener("click", () => {
 
 hamburgerX.addEventListener('click', () => {
     const togglebtnX = hamburgerX.classList.toggle('active');
+    portfolioName.style.display = 'flex';
     if (togglebtnX) {
         navUl.style.display = 'flex';
+        
     }
     else {
         navUl.style.display = 'none';
@@ -31,6 +39,7 @@ hamburgerX.addEventListener('click', () => {
 })
 
 navLinks.forEach(item => item.addEventListener("click", () => {
+    portfolioName.style.display = 'flex';
     navUl.style.display = 'none'
     hamburger.classList.remove("active");
     hamburgerX.classList.remove("active");
@@ -38,3 +47,18 @@ navLinks.forEach(item => item.addEventListener("click", () => {
     hamburger.style.display ='block';
     hamburgerX.style.display = 'none';
 }))
+
+
+// navLinks.forEach(item => item.addEventListener("click", () => {
+//     let width = '768px'
+//     if (navUl.style.display = 'none'){
+//         // navUl.style.display= 'flex';
+//         hamburgerX.style.display = 'none';
+//     }
+//     // navUl.style.display= 'flex'
+//     // hamburger.classList.remove("active");
+//     // hamburgerX.classList.remove("active");
+//     // navUl.classList.remove('active');
+//     // hamburger.style.display ='block';
+//     // hamburgerX.style.display = 'none';
+// }))
