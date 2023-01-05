0970b1fe73b5be7594be5d38f1e927642056ecc0
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-05 23:12:49 +0000

    Begin form validation

---
+++ form-validate.js
@@ -0,0 +1,13 @@
+const email = document.querySelectorAll('.input-individuals');
+console.log(email)
+const form = document.querySelector('.form-container')
+
+form.addEventListener('submit', (e) => {
+    let container = [];
+
+    let enteredEmail = document.email.inner
+
+    if (email.value !== email.value.lower )
+    // if (email.)
+    e.preventDefault()
+})
