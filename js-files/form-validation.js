4fa72a7aeda7c11662563856346afa8c9546fd6b
Loic <72917658+Hirwacardinalloic@users.noreply.github.com>
2023-01-06 13:01:56 +0200

    Change function name using camel case

--- js-files/form-validation.js
+++ js-files/form-validation.js
@@ -1,26 +1,26 @@
-// const form = document.getElementById('form');
+const form = document.getElementById('form');
 
-// function validateEmail(event) {
-//   // Get the value of the email field
-//   const email = document.getElementById('email').value;
-//   const errorParagraph = document.querySelector('.form-error');
+function checkUpperCase(event) {
+  // Get the value of the email field
+  const email = document.getElementById('email').value;
+  const errorParagraph = document.querySelector('.form-error');
 
-//   if (errorParagraph.querySelector('p') !== null) {
-//     const paragraph = errorParagraph.querySelector('p');
-//     errorParagraph.removeChild(paragraph);
-//   }
+  if (errorParagraph.querySelector('p') !== null) {
+    const paragraph = errorParagraph.querySelector('p');
+    errorParagraph.removeChild(paragraph);
+  }
 
-//   // Check if the email is in lower case
-//   if (email.toLowerCase() !== email) {
-//     // If the email is not in lower case, show an error message
-//     const errorMsg = 'Please your email address should be in lower casing';
-//     errorParagraph.appendChild(document.createElement('p')).innerHTML = errorMsg;
-//     errorParagraph.style.display = 'block';
-//     event.preventDefault();
-//   }
+  // Check if the email is in lower case
+  if (email.toLowerCase() !== email) {
+    // If the email is not in lower case, show an error message
+    const errorMsg = 'Please your email address should be in lower casing';
+    errorParagraph.appendChild(document.createElement('p')).innerHTML = errorMsg;
+    errorParagraph.style.display = 'block';
+    event.preventDefault();
+  }
 
-//   // If the email is in lower case, the form can be submitted
-//   return true;
-// }
+  // If the email is in lower case, the form can be submitted
+  return true;
+}
 
-// form.addEventListener('submit', validateEmail);
+form.addEventListener('submit', checkUpperCase);
