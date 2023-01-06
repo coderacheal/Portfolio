2f0fb38adce253a143b80b2409407cfbdeb04950
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-06 13:28:08 +0000

    Complete email capitalization check in form

--- js-files/form-validation.js
+++ js-files/form-validation.js
@@ -1,25 +1,28 @@
-const form = document.getElementById('form');
+const form = document.querySelector('.form-container');
+const submitButton = document.getElementById('get-in-touch-button');
 
 function checkUpperCase(event) {
-  // Get the value of the email field
-  const email = document.getElementById('email').value;
-  const errorParagraph = document.querySelector('.form-error');
+    const email = document.getElementById('email').value;
+    const errorMesssageDiv = document.querySelector('.error-message-div')
 
-  if (errorParagraph.querySelector('p') !== null) {
-    const paragraph = errorParagraph.querySelector('p');
-    errorParagraph.removeChild(paragraph);
-  }
+    if (errorMesssageDiv.querySelector('p') !== null) {
+        const paragraph = errorMesssageDiv.querySelector('p');
+        errorMesssageDiv.removeChild(paragraph);
+        errorMesssageDiv.style.display = 'none';
+        submitButton.style.marginTop = '40px';
+    }
 
-  // Check if the email is in lower case
-  if (email.toLowerCase() !== email) {
-    // If the email is not in lower case, show an error message
-    const errorMsg = 'Please your email address should be in lower casing';
-    errorParagraph.appendChild(document.createElement('p')).innerHTML = errorMsg;
-    errorParagraph.style.display = 'block';
-    event.preventDefault();
-  }
+    if (email.toLowerCase() !== email) {
+        const errorMessage = 'Please make sure that your emails do not contain any upper case characters';
+        errorMesssageDiv.appendChild(document.createElement('p')).innerHTML = errorMessage;
+        errorMesssageDiv.style.display = 'flex';
+        submitButton.style.marginTop = '70px';
+        event.preventDefault()
+    }
 
-  // If the email is in lower case, the form can be submitted
+    
+
+  
   return true;
 }
 
